018dc619d875f74c6dac375dc9c6361b66e06759
0joseDark <josegrand65@gmail.com>
2024-10-01 13:01:30 +0100

    update

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask import Flask, render_template
 # Criar a aplicação Flask
 app = Flask(__name__)
 
-# Rota para a página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Rota para a página de módulos do Windows
 @app.route('/modulos_windows')
 def modulos_windows():
     return render_template('modulos_windows.html')
 
-# Rota para a página de módulos do Linux
 @app.route('/modulos_linux')
 def modulos_linux():
     return render_template('modulos_linux.html')
 
-# Iniciar o servidor
+@app.route('/modulos_janelas')
+def modulos_janelas():
+    return render_template('modulos_janelas.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
